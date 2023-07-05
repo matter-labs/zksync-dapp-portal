e9efdb46b2aadb9130bb9be59a31add2daba6787
pcheremu <51121511+pcheremu@users.noreply.github.com>
2023-07-05 11:46:20 +0200

    test: fixed env vars

--- tests/e2e/src/pages/login.page.ts
+++ tests/e2e/src/pages/login.page.ts
@@ -1,7 +1,7 @@
 import { BasePage } from "./base.page";
 import { MetamaskPage } from "./metamask.page";
-import { MetamaskWallet } from "../data/data";
-import { config } from "../support/config";
+import { Helper } from "../helpers/helper";
+import { config, wallet } from "../support/config";
 
 import type { ICustomWorld } from "../support/custom-world";
 
@@ -15,6 +15,7 @@ export class LoginPage extends BasePage {
   }
 
   async connectMetamask() {
+    const helper = await new Helper(this.world);
     const loginStatus = await this.checkLoginStatus();
     if (!loginStatus) {
       const metamaskPage = await new MetamaskPage(this.world);
@@ -26,7 +27,7 @@ export class LoginPage extends BasePage {
         ?.locator(metamaskPage.unlockPasswordField)
         .isVisible(config.defaultTimeout);
       if (passwordFieldVisible) {
-        await popUp?.locator(metamaskPage.unlockPasswordField).fill(MetamaskWallet.mainWalletPassword);
+        await popUp?.locator(metamaskPage.unlockPasswordField).fill(await helper.decrypt(wallet.password));
         await popUp?.locator(metamaskPage.confirmUnlockBtn).click();
       }
       await popUp?.waitForTimeout(700);
