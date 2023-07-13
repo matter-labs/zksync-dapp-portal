1f3b548fbdc0a5703714fa0610fe425c8cf74c74
Jack Hamer <jackhamer0990@gmail.com>
2023-07-13 17:32:42 +0300

    feat: add era swaps

--- pages/transaction/zksync/era/swap.vue
+++ pages/transaction/zksync/era/swap.vue
@@ -5,11 +5,68 @@
 
     <CommonCardWithLineButtons>
       <DestinationItem
-        v-bind="destinations.syncswap"
+        label="Mute.io"
+        icon-url="/img/mute.svg"
+        :icon="ArrowUpRightIcon"
+        as="a"
+        target="_blank"
+        href="https://app.mute.io/swap"
+      />
+      <DestinationItem
+        label="Maverick Protocol"
+        icon-url="/img/maverick-protocol.svg"
+        :icon="ArrowUpRightIcon"
+        as="a"
+        target="_blank"
+        href="https://app.mav.xyz/?chain=324"
+      />
+      <DestinationItem
+        label="iZiSwap"
+        icon-url="/img/izumi.svg"
+        :icon="ArrowUpRightIcon"
+        as="a"
+        target="_blank"
+        href="https://izumi.finance/trade/swap"
+      />
+      <DestinationItem
+        label="Velocore"
+        icon-url="/img/velocore.svg"
+        :icon="ArrowUpRightIcon"
+        as="a"
+        target="_blank"
+        href="https://app.velocore.xyz/swap"
+      />
+      <DestinationItem
+        label="Syncswap"
+        icon-url="/img/syncswap.svg"
         :icon="ArrowUpRightIcon"
         as="a"
         target="_blank"
         href="https://syncswap.xyz/"
+      />
+      <DestinationItem
+        label="SpaceFi"
+        icon-url="/img/spacefi.svg"
+        :icon="ArrowUpRightIcon"
+        as="a"
+        target="_blank"
+        href="https://swap-zksync.spacefi.io/#/swap"
+      />
+      <DestinationItem
+        label="eZKalibur"
+        icon-url="/img/ezkalibur.svg"
+        :icon="ArrowUpRightIcon"
+        as="a"
+        target="_blank"
+        href="https://dapp.ezkalibur.com/"
+      />
+      <DestinationItem
+        label="veSync"
+        icon-url="/img/vesync.svg"
+        :icon="ArrowUpRightIcon"
+        as="a"
+        target="_blank"
+        href="https://app.vesync.finance/swap"
       />
     </CommonCardWithLineButtons>
   </div>
@@ -17,11 +74,6 @@
 
 <script lang="ts" setup>
 import { ArrowUpRightIcon } from "@heroicons/vue/24/outline";
-import { storeToRefs } from "pinia";
-
-import { useDestinationsStore } from "@/store/destinations";
-
-const { destinations } = storeToRefs(useDestinationsStore());
 </script>
 
 <style lang="scss" scoped></style>
