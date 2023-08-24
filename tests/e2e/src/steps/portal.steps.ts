0c669e8b62b158f28e4bd01d175c9a7126671fe0
pcheremu <pavel.cheremushkin@gmail.com>
2023-08-24 18:51:22 +0200

    test: fix for the logic for empty walltes - rebase

--- tests/e2e/src/steps/portal.steps.ts
+++ tests/e2e/src/steps/portal.steps.ts
@@ -393,6 +393,8 @@ Then("Faucet tokens animation should be visible", config.stepTimeout, async func
 
 Given("I reset allowance", config.stepExtraTimeout, async function (this: ICustomWorld) {
   revokePage = new RevokePage(this);
+  metamaskPage = new MetamaskPage(this);
+  await metamaskPage.extractCurrentWalletAddress();
   await revokePage.login();
   await revokePage.revokeAllowance();
   await revokePage.logout();
