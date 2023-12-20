0fdfb5b3f5118a508f52eb48f9f557ab08162136
Jack Hamer <jackhamer0990@gmail.com>
2023-12-20 14:22:53 +0200

    fix: 404 page

--- error.vue
+++ error.vue
@@ -20,12 +20,9 @@ export default {
   @apply grid grid-rows-[auto_1fr];
   min-height: 100vh;
   min-height: 100dvh;
-  grid-template-areas:
-    "header header header header"
-    "menu menu menu menu";
 
   .error-info-container {
-    @apply col-span-4 mx-auto flex h-full w-11/12 max-w-md flex-col items-center justify-center px-4 py-2 leading-normal md:px-0 md:py-4;
+    @apply mx-auto flex h-full w-11/12 max-w-md flex-col items-center justify-center px-4 py-2 leading-normal md:px-0 md:py-4;
 
     .error-status-code {
       @apply mb-2 text-9xl font-normal;
